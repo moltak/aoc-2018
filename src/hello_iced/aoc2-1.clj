22a0954a0a726471d2667dc69bf8d65db6ec45e2
kh <moltak@gmail.com>
2022-10-17 15:27:15 +0900

    aoc1-2

---
+++ src/hello_iced/aoc2-1.clj
@@ -0,0 +1,13 @@
+(ns hello-iced.aoc2-1)
+
+"https://github.com/green-labs/bootcamp-aoc/blob/main/translations/aoc2018/day2.md"
+"
+abcdef 어떤 문자도 두 번 혹은 세 번 나타나지 않음 -> (두 번 나오는 문자 수: 0, 세 번 나오는 문자 수: 0)
+bababc 2개의 a, 3개의 b -> (두 번 나오는 문자 수: 1, 세 번 나오는 문자 수: 1)
+abbcde 2개의 b -> (두 번 나오는 문자 수: 1, 세 번 나오는 문자 수: 0)
+abcccd 3개의 c -> (두 번 나오는 문자 수: 0, 세 번 나오는 문자 수: 1)
+aabcdd 2개의 a, 2개의 d -> (두 번 나오는 문자 수: 2, 세 번 나오는 문자 수: 0)
+abcdee 2개의 e -> (두 번 나오는 문자 수: 1, 세 번 나오는 문자 수: 0)
+ababab 3개의 a, 3개의 b -> (두 번 나오는 문자 수: 0, 세 번 나오는 문자 수: 2)
+"
+
--- src/hello_iced/aoc2.clj
+++ src/hello_iced/aoc2.clj
@@ -1,4 +1,7 @@
-(ns hello-iced.aoc2)
+(ns hello-iced.aoc2
+ (:require
+   [clojure.string :as str]))
+
 
 ; https://github.com/green-labs/bootcamp-aoc/blob/main/translations/aoc2018/day1.md
 ; 주어진 입력의 숫자를 더할 때마다 나오는 숫자 중, 처음으로 두 번 나오는 숫자를 리턴하시오.
@@ -7,25 +10,39 @@
 
 (def array [3, 3, 4, -1, -4])
 
+(def input-file (slurp "resources/aoc1-2.input"))
+
+
 ; 1. java object 사용
-; 2. loop, recur 사용
+; 2. loop, recur 사r
 
 (defn solve [array]
-  (loop [res-set #{} sum 0 item (first array)]
-    (println res-set " -> " item)
+  (loop [res-set #{} sum 0 xs array]
+    ;(println res-set " -> " (first xs))
     (if (contains? res-set sum) 
       sum
       (recur
-        (conj res-set #{sum})
-        (+ sum item)
-        (first (rest array))
+        (conj res-set sum)
+        (+ sum (first xs))
+        (rest xs)
       )
     )
   )
 )
 
 (comment 
-  ;(solve (take 20 (cycle array))) ; 10
+  (->> input-file 
+       str/split-lines
+       (map parse-long) 
+       cycle
+       solve
+  )
+  
+  (solve (take 20 (cycle array))) ; 10
+  (solve (take 20 (cycle [+1 -1]))) ; 10
+  (solve (take 20 (cycle [+7, +7, -2, -7, -4]))) ; 10
+  (solve (take 20 (cycle [-6, +3, +8, +5, -6]))) ; 10
+  (solve (take 20 (cycle array))) ; 10
   (solve array)
   (conj #{1 2 3} 3)
   (#{1 2 3} 4)
