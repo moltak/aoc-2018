0903489f010c05e56df17fdf70f7c7ca30be177e
kh <moltak@gmail.com>
2022-10-22 15:57:54 +0900

    WIP 4-1

--- src/hello_iced/aoc4_1.clj
+++ src/hello_iced/aoc4_1.clj
@@ -1,5 +1,6 @@
 (ns hello-iced.aoc4-1
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.test :as test]))
 (use 'clojure.set)
 
 (comment "
@@ -36,10 +37,85 @@
 파트 2
 주어진 분(minute)에 가장 많이 잠들어 있던 가드의 ID과 그 분(minute)을 곱한 값을 구하라.")
 
+
+(def test-input ["[1518-11-01 00:00] Guard #10 begins shift"
+                 "[1518-11-01 00:55] wakes up"
+                 "[1518-11-01 23:58] Guard #99 begins shift"
+                 "[1518-11-02 00:40] falls asleep"
+                 "[1518-11-02 00:50] wakes up"
+                 "[1518-11-03 00:05] Guard #10 begins shift"
+                 "[1518-11-03 00:24] falls asleep"
+                 "[1518-11-03 00:29] wakes up"
+                 "[1518-11-04 00:02] Guard #99 begins shift"
+                 "[1518-11-04 00:36] falls asleep"
+                 "[1518-11-04 00:46] wakes up"
+                 "[1518-11-05 00:03] Guard #99 begins shift"
+                 "[1518-11-01 00:05] falls asleep"
+                 "[1518-11-01 00:25] wakes up"
+                 "[1518-11-01 00:30] falls asleep"
+                 "[1518-11-05 00:45] falls asleep"
+                 "[1518-11-05 00:55] wakes up"])
+
+
 (def input4-1 (str/split-lines (slurp "resources/aoc4_1.input")))
 
+(defn to-timeschedule
+  "문제에서 정의한 형식의 코드 한 줄을 받아 스케줄 표로 변환합니다."
+  [raw-code]
+  (let [[date] (re-find #"(\[[^\[\]]+\])" raw-code)
+        [guard] (re-find #"(\#\d+)" raw-code)
+        [asleep] (re-find #"(falls asleep)" raw-code)
+        [wake-up] (re-find #"(wakes up)" raw-code)
+        ]
+    {
+     :date (str/replace date #"\[|\]" "")
+     :guard guard
+     :asleep asleep
+     :wake_up wake-up
+     }))
+
+
 (comment
-  
-  
-  
-  )
+  (to-timeschedule (first test-input))
+  (->> test-input
+       (map to-timeschedule)
+       (sort (fn [a b] (compare (a :date) (b :date))))))
+
+(defn for-guard-schedule
+  "가드의 시간표로 묶어서 표현합니다.
+  [1518-11-01 00:00] Guard #10 begins shift
+  [1518-11-01 00:05] falls asleep
+  [1518-11-01 00:25] wakes up
+  [1518-11-01 00:30] falls asleep
+  [1518-11-01 00:55] wakes up
+
+  -> guard: #10, { :'1518-11-01 00:05' asleep  :'time' wakes up  :'time' falls asleep  :'time' wakes up
+  "
+  [schedules]
+  (->> schedules
+       (sort (fn [a b] (compare (a :date) (b :date))))
+       (reduce (fn [acc x] 
+                 (if (x :guard) 
+                   (conj acc x) 
+                   (conj acc (assoc x :guard ((last acc) :guard))))) 
+               [])))
+
+(comment 
+  (->> test-input 
+       (map to-timeschedule)
+       for-guard-schedule
+       (group-by :guard)))
+
+
+(defn 가드가-자는-수
+  [by-guard-schedules]
+  (->> by-guard-schedules
+       (filter (fn [x] (x :asleep)))
+       count))
+
+(comment 
+  (->> test-input 
+       (map to-timeschedule)
+       for-guard-schedule
+       (group-by :guard)
+       (map (fn [x] (가드가-자는-수 (second x))))))
